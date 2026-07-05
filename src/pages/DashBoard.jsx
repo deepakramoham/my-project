1f74af3ab0ea5d65a5e9586a486edf9f1277c729
Deepak Janardhanan <deepakramoham@gmail.com>
2026-07-05 15:10:17 +0530

    loading images using img tag from public folder and assets folder

--- src/pages/DashBoard.jsx
+++ src/pages/DashBoard.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import DashBoardImage from "../assets/projectImages/projectImage.jpg";
 
 const DashBoard = () => {
-  return <div>DashBoard</div>;
+  return (
+    <div>
+      <div>Dashboard</div>
+      {/* <img src={DashBoardImage} alt="" /> */}
+      {/* <img src="images/someImage.jpg" alt="" /> */}
+    </div>
+  );
 };
 
 export default DashBoard;
